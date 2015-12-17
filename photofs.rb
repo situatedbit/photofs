c34bd39d9d2b4e93464e641cfe2a0d5beb8a0e48
Matt Schaefer <matt@situatedbit.com>
2015-12-17 22:20:25 +0900

    can create links within virtual space

--- photofs.rb
+++ photofs.rb
@@ -1,5 +1,36 @@
 require 'rfuse'
 
+class VirtualStat < RFuse::Stat
+  DEFAULT_PERMISSIONS = 0400
+
+  def initialize(real_file_abs_path)
+    real_file_stat = File.stat(real_file_abs_path)
+
+    # list of attributes
+    attributes = [ :atime, :blksize, :blocks, :ctime, :dev, 
+                   :gid, :ino, :mode, :mtime, :nlink, :rdev, 
+                   :size, :uid ]
+
+    attr_hash = {}
+
+    attributes.map do | attribute |
+      attr_hash[attribute] = real_file_stat.send attribute
+    end
+
+    if File.directory? real_file_abs_path
+      type = RFuse::Stat::S_IFDIR
+    else
+      # make the file a symbolic link to the real file
+      type = RFuse::Stat::S_IFLNK
+      attr_hash[:mode] = RFuse::Stat::S_IFLNK | DEFAULT_PERMISSIONS
+      attr_hash[:nlink] = 1
+      attr_hash[:size] = real_file_abs_path.length
+    end
+
+    super(type, DEFAULT_PERMISSIONS, attr_hash)
+  end
+end
+
 class PhotoFS
   attr_reader :root
 
@@ -19,20 +50,27 @@ class PhotoFS
   end
 
   public
-  def readdir(ctx, path, filler, offset, ffi)
+  def readdir(context, path, filler, offset, ffi)
     log "readdir: #{path}"
-    full_path = @root + path
+    full_path = File.absolute_path(@root + path)
 
     raise Errno::ENOTDIR.new(full_path) unless File.directory? full_path
 
     Dir.entries(full_path).each do |entry|
-      filler.push(entry, File.stat(full_path), 0)
+      filler.push(entry, VirtualStat.new(full_path), 0)
     end
   end
 
-  def getattr(ctx, path)
+  def getattr(context, path)
     log "stat: #{path}"
-    File.stat(@root + path)
+
+    VirtualStat.new(File.absolute_path(@root + path))
+  end
+
+  def readlink(context, path, size)
+    log "readlink: #{path}, #{size.to_s}"
+
+    File.absolute_path(@root + path)[0, size]
   end
 
   def log(s)
