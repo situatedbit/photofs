f1fe4cc3ed03703d238280ee48d0936d14d84ea9
Matt Schaefer <matt@situatedbit.com>
2016-01-03 20:12:25 -0600

    cleaned out comments

--- photofs.rb
+++ photofs.rb
@@ -1,48 +1,6 @@
 require 'rfuse'
-
 require_relative 'lib/mirrored_dir'
 
-=begin
-class Fuse
-  readdir
-    node = find(path)
-    if node.directory?
-      node.entries.each { ... entry.name, entry.stat, ... }
-
-  readlink
-    node = top_level_dir(path).find(path)
-    if node.link?
-      node.target
-end
-
-# file < node
-  # target
-
-# mirror-dir < dir
-# categories-dir < dir
-
-=end
-=begin
-class VirtualStat < RFuse::Stat
-  DEFAULT_PERMISSIONS = 0000400 # read only by owner
-
-  def initialize(real_file_abs_path)
-    attr_hash = PhotoFS::Stat.stat_hash(File.stat(real_file_abs_path))
-
-    if File.directory? real_file_abs_path
-      type = RFuse::Stat::S_IFDIR
-    else
-      # make the file a symbolic link to the real file
-      type = RFuse::Stat::S_IFLNK
-      attr_hash[:mode] = RFuse::Stat::S_IFLNK | DEFAULT_PERMISSIONS
-      attr_hash[:nlink] = 1
-      attr_hash[:size] = real_file_abs_path.length
-    end
-
-    super(type, DEFAULT_PERMISSIONS, attr_hash)
-  end
-end
-=end
 module PhotoFS
   class Fuse
     attr_reader :source_path
@@ -83,10 +41,7 @@ module PhotoFS
 
       raise Errno::ENOTDIR.new(path) unless dir.directory?
 
-      dir.nodes.each do |n| 
-        puts n.name, n.stat
-        filler.push(n.name, n.stat, 0)
-      end
+      dir.nodes.each { |n| filler.push(n.name, n.stat, 0) }
     end
 
     def getattr(context, path)
