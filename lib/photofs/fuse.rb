e375efe673888e9566b5dbe2058ac6f0d5c9dd9d
Matt Schaefer <matt@situatedbit.com>
2016-03-25 16:08:37 +0900

    moved mounting logic into fuse module

--- lib/photofs/fuse.rb
+++ lib/photofs/fuse.rb
@@ -1,1 +1,16 @@
 require_relative 'fuse/fuse'
+
+module PhotoFS
+  module Fuse
+    MY_OPTIONS = [:source]
+    OPTION_USAGE = " -o source=path/to/photos/"
+
+    # Usage: #{$0} mountpoint [mount_options] -o source=/path/to/photos
+    def self.mount
+      RFuse.main(ARGV, MY_OPTIONS, OPTION_USAGE, nil, $0) do |options|
+        PhotoFS::Fuse::Fuse.new options
+      end
+    end
+
+  end
+end
--- photofs.rb
+++ photofs.rb
@@ -1,13 +1,6 @@
 libdir = File.join(File.dirname(__FILE__), 'lib')
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-require 'rfuse'
 require 'photofs/fuse'
 
-MY_OPTIONS = [:source]
-OPTION_USAGE = " -o source=path/to/photos/"
-
-# Usage: #{$0} mountpoint [mount_options] -o source=/path/to/photos
-RFuse.main(ARGV, MY_OPTIONS, OPTION_USAGE, nil, $0) do |options|
-  PhotoFS::Fuse::Fuse.new options
-end
+PhotoFS::Fuse.mount
