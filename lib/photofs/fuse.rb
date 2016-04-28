cc3e3d606b338e4f7ba00a67dfb2f707abb9ed58
Matt Schaefer <matt@situatedbit.com>
2016-04-28 11:13:54 +0900

    daemonize process

--- lib/photofs/fuse.rb
+++ lib/photofs/fuse.rb
@@ -2,12 +2,14 @@ require_relative 'fuse/fuse'
 
 module PhotoFS
   module Fuse
-    MY_OPTIONS = [:source]
+    MY_OPTIONS = [:daemon, :source]
     OPTION_USAGE = " -o source=path/to/photos/"
 
     # Usage: #{$0} mountpoint [mount_options] -o source=/path/to/photos
     def self.mount
       RFuse.main(ARGV, MY_OPTIONS, OPTION_USAGE, nil, $0) do |options|
+        Process.daemon if options[:daemon]
+
         PhotoFS::Fuse::Fuse.new options
       end
     end
