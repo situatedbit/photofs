bd80627591189f61d0f29b30462e906343fea996
Matt Schaefer <matt@situatedbit.com>
2016-06-07 08:30:44 +0900

    removed tests meant to optimize database version of tag set

--- spec/lib/photofs/data/tag_set_spec.rb
+++ spec/lib/photofs/data/tag_set_spec.rb
@@ -76,63 +76,6 @@ describe PhotoFS::Data::TagSet do
     end
   end # :delete
 
-  describe :find_by_images do
-    let(:image_set) { PhotoFS::Core::ImageSet.new :set => [image].to_set }
-    let(:image_record) { create_image '一.jpg' }
-    let(:image) { image_record.to_simple }
-
-    let(:tag_record) { create(:tag, :name => 'タグ') }
-    let(:tag) { tag_record.to_simple }
-
-    context "when there are no tags" do
-      it "should return an empty collection" do
-        expect(tag_set.find_by_images image_set).to be_empty
-      end
-    end
-
-    context 'when there are no images' do
-      before(:each) do
-        tag_record.images << image_record
-        tag_record.save
-      end
-
-      it { expect(tag_set.find_by_images PhotoFS::Core::ImageSet.new).to be_empty }
-    end
-
-    context "when passed a single image" do
-      before(:each) do
-        tag_record.images << image_record
-        tag_record.save
-      end
-
-      it "should return the tags for that image" do
-        expect(tag_set.find_by_images image_set).to contain_exactly(tag)
-      end
-    end
-
-    context "when there are multiple images" do
-      let(:image2_record) { create_image '二.jpg' }
-      let(:image2) { image2_record.to_simple  }
-      let!(:image_set) { PhotoFS::Core::ImageSet.new :set => [image, image2].to_set }
-
-      let!(:tag_record) { create(:tag, :name => 'タグ') }
-      let!(:tag2_record) { create :tag, :name => '三' }
-      let!(:tag3_record) { create :tag, :name => '四' }
-
-      before(:each) do
-        tag_record.images << [image_record, image2_record]
-        tag_record.save
-
-        tag2_record.images << [image_record, image2_record]
-        tag2_record.save
-      end
-
-      it "should return the union of tags from those images" do
-        expect(tag_set.find_by_images image_set).to contain_exactly(tag_record.to_simple, tag2_record.to_simple)
-      end
-    end
-  end # :find_by_images
-
   describe :save! do
     it 'should call the Data module method' do
       expect(tag_set).to receive(:save_record_object_map).with(tag_set.instance_variable_get(:@record_object_map))
