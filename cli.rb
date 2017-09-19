52a00ad26474c46eb6ed7ed964aebb5e2ecff0b5
Matt Schaefer <matt@situatedbit.com>
2017-09-19 13:24:01 -0700

    moved cli files to different repo

--- cli.rb
+++
@@ -1,6 +0,0 @@
-libdir = File.join(File.dirname(__FILE__), 'lib')
-$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
-
-require 'photofs/cli'
-
-PhotoFS::CLI.execute ARGV
--- lib/photofs/cli.rb
+++
@@ -1,1 +0,0 @@
-require 'photofs/cli/cli'
