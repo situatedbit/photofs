16d927491cca564d069e794dbd9a6e0dfa3c39da
Matt Schaefer <matt@situatedbit.com>
2017-11-22 07:51:02 -0800

    added version number to migrations

--- db/migrate/20160403010010_rename_jpeg_file_column.rb
+++ db/migrate/20160403010010_rename_jpeg_file_column.rb
@@ -1,4 +1,4 @@
-class RenameJpegFileColumn < ActiveRecord::Migration
+class RenameJpegFileColumn < ActiveRecord::Migration[4.2]
   def change
     rename_column :images, :jpeg_file_id, :image_file_id
   end
