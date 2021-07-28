b95dddab7b8ef8cc1d91ede50014b9dc9731355e
Matt Schaefer <matt@situatedbit.com>
2021-07-28 08:31:25 -0700

    added error messaging when mount source does not exist

--- lib/photofs/fuse.rb
+++ lib/photofs/fuse.rb
@@ -8,8 +8,14 @@ module PhotoFS
     # Usage: #{$0} mountpoint [mount_options] -o source=/path/to/photos
     def self.mount
       RFuse.main(ARGV, MY_OPTIONS, OPTION_USAGE, nil, $0) do |options|
-        # get path before daemonizing, after which we'd lose CWD, relatives paths like ~
-        options[:source] = options.has_key?(:source) ? ::File.realpath(::File.expand_path(options[:source])) : nil
+        raise RuntimeError.new('Missing required source option') unless options.has_key?(:source)
+
+        begin
+          # get path before daemonizing, after which we'd lose CWD, relatives paths like ~
+          options[:source] = ::File.realpath(::File.expand_path(options[:source]))
+        rescue Errno::ENOENT
+          raise "Source path does not exist: #{options[:source]}"
+        end
 
         Process.daemon if options[:daemon]
 
